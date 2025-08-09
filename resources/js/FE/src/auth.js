8ecd7a616e6cdb33432e1413bb044293577c45a9
haidev306 <haidev306@gmail.com>
2025-08-09 22:37:33 +0700

    Add secret key configuration for NextAuth in auth.js

--- resources/js/FE/src/auth.js
+++ resources/js/FE/src/auth.js
@@ -3,6 +3,7 @@ import appConfig from '@/configs/app.config'
 import authConfig from '@/configs/auth.config'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+    secret: process.env.NEXTAUTH_SECRET || 'default-secret-key',
     pages: {
         signIn: appConfig.unAuthenticatedEntryPath,
         error: appConfig.unAuthenticatedEntryPath,
